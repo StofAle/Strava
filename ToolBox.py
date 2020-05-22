756049c7aa9693776d74368c08dcf6f54e7429b4
AlexS <54623324+StofAle@users.noreply.github.com>
2020-05-21 20:37:10 -0400

    updates to notebook and ToolBox; data downloaded

--- ToolBox.py
+++ ToolBox.py
@@ -4,14 +4,6 @@ version: 0.5
 date: 05/19/2020
 author: Alexander Stoffers
 
-info:
-
-class methods:
-- update
--
--
-
-
 """
 
 # load modules
@@ -21,12 +13,36 @@ import json
 import os
 from sklearn.neighbors import LocalOutlierFactor
 
+
 class Strava_ToolBox():
     """
-    ???update description
+    This class is used to obtain and analyze running data sourced through the strava api.
+    For more information on the strava api, see https://www.strava.com/settings/api
 
-    ToDo:
-    make unit time and cutoffs accessible from the outside
+    The following functionality is available:
+
+    1/ (down)load data from API or local folder
+
+        raw and clean data is stored as an attribute
+
+        available methods:
+            - get_activities()
+            - load_files()
+
+
+    2/ data cleaning and (pre)processing
+
+        available methods and auxiliary functions to clean data and get run metrics:
+            - save_clean_data()
+            - get_data_type_from_file()
+            - get_velocities()
+            - get_activity_stats()
+            - process_raw_data()
+            - instantaneous_velocity()
+            - get_instantaneous_velocities()
+            - get_clean_data()
+
+
     """
 
     def __init__(self):
@@ -51,8 +67,12 @@ class Strava_ToolBox():
     def get_activities(client, path: str, limit: int = 1000) -> None:
         """ get and save running data from strava API to json files
 
-        :param client:
-        :type client:
+        :param client: strava client
+        :type client: stravalib.client.Client
+        :param path: output folder
+        :type path: str
+        :param limit: maximum number of activities attempted to download
+        :type limit: int
         :return: None
         :rtype: None
 
@@ -85,12 +105,19 @@ class Strava_ToolBox():
                     dict_ = {'times': times, 'distances': distances, 'infos': infos}
 
                     # save data
-                    with open(filename + '.json', 'w') as fp:
+                    with open(path + filename, 'w') as fp:
                         json.dump(dict_, fp)
 
     def save_clean_data(self, path: str) -> None:
+        """ save dataframe containing clean data to file
+
+        :param path: output folder
+        :type path: str
+        :return: None
+        :rtype: None
         """
-        """
+
+        # save each activity in the clean data dictionary
         for key in self.clean_data.keys():
             filename = str(key) + '.json'
             data = self.clean_data[key]
@@ -100,8 +127,15 @@ class Strava_ToolBox():
         return None
 
     def load_files(self, path: str, workout_type: str = 'Run') -> None:
-        """
-        ???update description
+        """ load activity files from path
+
+        :param path: folder
+        :type path: str
+        :param workout_type: type of workout. Default is 'Run'
+        :type workout_type: str
+        :return: None
+        :rtype: None
+
         """
 
         if self.raw_data and self.clean_data:
@@ -133,8 +167,17 @@ class Strava_ToolBox():
         return None
 
     def get_data_type_from_file(data: dict) -> str:
+        """ get type of activity from data dictionary
+
+        this function is consumed in the method load_files()
+
+        :param data: raw or clean data dictionary
+        :type data: dict
+        :return: type of data
+        :rtype: str
+
         """
-        """
+
         try:
             if data['infos']['data_type']:
                 return data['infos']['data_type']
@@ -148,25 +191,44 @@ class Strava_ToolBox():
             return ''
 
     def get_velocities(self, times: list, distances: list) -> [float, float, float]:
+        """ given a list of times and distances, the corresponding (instanteneous) velocity is calculated
+
+        Note:
+        The recorded Strava data is not equidistant in time and distance units.
+
+        This function is used in the method get_activity_stats()
+
+        :param times: list of time stamps
+        :type times: list
+        :param distances: list of distances
+        :type distances: list
+        :return: average velocity, time and distance
+        :rtype: list of three floats
+
         """
-        update description
-        """
+
+        # initialize temporary lists
         velocities = []
         velocity_times = []
         velocity_distances = []
 
+        # iterate over input time list
         times_iter = iter(times)
         while True:
 
             try:
                 start_time = next(times_iter)
 
+                # take only time intervals after cutoff_beginning and before cutoff_end
+                # logic: strava recording is most of the time started before I actually start running and
+                # its stopped after I've stopped running
                 if self.cutoff_beginning < start_time < times[-1] - self.cutoff_end:
                     time = start_time
                     sum_velocity = 0
                     sum_time = 0
                     sum_distance = 0
 
+                    # aggregate distances, times and velocities
                     j = 0
                     while time < start_time + self.unit_time and time + self.unit_time <= times[-1]:
                         delta_distance = distances[j + 1] - distances[j]
@@ -178,6 +240,7 @@ class Strava_ToolBox():
                         j = j + 1
                         time = times[j]
 
+                    # calculate averaged quantities from aggregated data
                     if sum_time > 0:
                         avg_velocity = sum_velocity / float(j)
                         avg_time = sum_time / float(j)
@@ -192,10 +255,18 @@ class Strava_ToolBox():
         return velocities, velocity_times, velocity_distances
 
     def get_activity_stats(self, activity_id: int) -> dict:
-        """
-        ???update description
+        """ compute various metrics for given activity
+
+        this helper function is used in the method process_raw_data()
+
+        :param activity_id: activityID
+        :type activity_id: int
+        :return: dictionary containing activity stats
+        :rtype: dict
+
         """
 
+        # initialize output dictionary
         dict_ = {}
 
         if self.use_outlier_filtering:
@@ -211,17 +282,23 @@ class Strava_ToolBox():
         velocities, velocity_times, velocity_distances = self.get_velocities(
             times=times, distances=distances)
 
+        # calculate metrics and save in dictionary
         dict_['avg_velocity'] = np.average(velocities)
         dict_['std_velocity'] = np.std(velocities, axis=0)
-        dict_['total_duration'] = velocity_times[-1]
-        dict_['total_distance'] = velocity_distances[-1]
-        dict_['start_date_local'] = self.raw_data[activity_id]['infos']['start_date_local']
+        dict_['total_duration'] = times[-1]
+        dict_['total_distance'] = distances[-1]
+        tmp_date = self.raw_data[activity_id]['infos']['start_date_local'].split('T')[0]
+        tmp_date = tmp_date.split('-')
+        tmp_date = tmp_date[0] + tmp_date[1] + tmp_date[2]
+        dict_['start_date_local'] = tmp_date
 
         return dict_
 
     def process_raw_data(self) -> None:
-        """
-        ???update description
+        """ get activity stats for all raw data
+
+        :return: None
+        :rtype: None
         """
 
         if not self.data.empty:
@@ -247,7 +324,14 @@ class Strava_ToolBox():
 
     @staticmethod
     def instantaneous_velocity(distances: list, times: list):
-        """
+        """ calculate instantaneous velocities given a list of distances and times
+
+        Note: Function yields instantaneous velocity and the start time point corresponding to the velocity
+
+        :param distances: list of distances
+        :type distances: list
+        :param times: list of times
+        :type times: list
         """
 
         distances_iter = iter(distances)
@@ -274,7 +358,12 @@ class Strava_ToolBox():
                 break
 
     def get_instantaneous_velocities(self, activity_id: int) -> np.ndarray:
-        """
+        """ calculate instantaneous velocities for a given activityID
+
+        :param activity_id: activityID
+        :type activity_id: int
+        :return: instantaneous velocities
+        :rtype: numpy array
         """
 
         distances = self.raw_data[activity_id]['distances']
@@ -288,23 +377,31 @@ class Strava_ToolBox():
         return inst_velocities
 
     def get_clean_data(self, activity_id: int) -> pd.DataFrame:
-        """
+        """ remove velocity outliers from raw data and save clean data in dataframe
+
+        this function is used in the method get_activity_stats()
+
+        :param activity_id: activityID
+        :type activity_id: int
+        :return: dataframe with clean data
+        :rtype: dataframe
         """
 
+        # use local outlier factor method to remove outliers in instantaneous velocities
         LOF = LocalOutlierFactor(n_neighbors=self.neighbors)
         X = self.get_instantaneous_velocities(activity_id)
         y_pred = LOF.fit_predict(X)
         X_scores = LOF.negative_outlier_factor_
 
-        df = pd.DataFrame({'time': X[:, 0], 'outlier_flg': y_pred
-                              , 'LOF_score': X_scores})
+        # merge results from LOF to raw data
+        df = pd.DataFrame({'time': X[:, 0], 'outlier_flg': y_pred, 'LOF_score': X_scores})
 
         df_raw = pd.DataFrame({'time': self.raw_data[activity_id]['times'],
                                'distances': self.raw_data[activity_id]['distances']})
 
         df = df.merge(df_raw, on='time')
 
-        if self.write_to_clean_data == True:
+        if self.write_to_clean_data:
             self.clean_data[activity_id] = df.to_dict()
             self.clean_data[activity_id]['infos'] = self.raw_data[activity_id]['infos']
             self.clean_data[activity_id]['infos']['data_type'] = 'clean_data'
